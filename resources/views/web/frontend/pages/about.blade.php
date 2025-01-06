d40f5573f3a5498165936905a5d90be7364dc558
karimgouda <dev.karimgouda@gmail.com>
2025-01-06 13:35:10 +0200

    Add

--- resources/views/web/frontend/pages/about.blade.php
+++ resources/views/web/frontend/pages/about.blade.php
@@ -72,37 +72,4 @@
     </div>
     <!-- About End -->
 
-    <!-- Team Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <div class="bg-primary mb-3 mx-auto" style="width: 60px; height: 2px"></div>
-                <h1 class="display-5 mb-5">{{__('pages.Team Members')}}</h1>
-            </div>
-            <div class="row g-4">
-                @foreach($workers as $key=>$worker)
-                    <div class="col-lg-3 col-md-6 wow fadeInUp" data-wow-delay="0.{{++$key}}s">
-                        <div class="team-item">
-                            <div class="overflow-hidden position-relative">
-                                <img class="img-fluid" src="{{public_storage($worker->image)}}" alt="" />
-                                <div class="team-social">
-                                    <a class="btn btn-square btn-dark rounded-circle m-1"
-                                       href="https://www.facebook.com/Eqraaserv1?mibextid=ZbWKwL"><i class="fab fa-facebook-f"></i></a>
-                                    <a class="btn btn-square btn-dark rounded-circle m-1" href=""><i class="fab fa-twitter"></i></a>
-                                    <a class="btn btn-square btn-dark rounded-circle m-1" href="https://www.instagram.com/Eqraaserv14/"><i
-                                            class="fab fa-instagram"></i></a>
-                                </div>
-                            </div>
-                            <div class="text-center p-4">
-                                <h5 class="mb-0">{{$worker->name}}</h5>
-                                <span class="text-primary">{{$worker->description}}</span>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-
-            </div>
-        </div>
-    </div>
-    <!-- Team End -->
 @endsection
