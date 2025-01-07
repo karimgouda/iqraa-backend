5a3131bbdb37d89f2b64fdaecd4fd5c37ae01792
Ali Talaat <alitalaatali30@gmail.com>
2025-01-07 17:35:14 +0200

    update

--- resources/views/web/frontend/layouts/head.blade.php
+++ resources/views/web/frontend/layouts/head.blade.php
@@ -3,15 +3,15 @@
     {!! settings('head_manager_script') !!}
 
     @include('web.frontend.layouts.partials._seo')
-    
+
     @include('web.frontend.layouts.head.meta')
-    
+
     <!-- Title -->
     <title> {{ settings('site_name_' . app()->getLocale()) }} - @yield('title') </title>
-    
+
     <!-- Favicon -->
     <link rel="shortcut icon" type="image/png" href="{{public_storage(settings('site_favicon'))}}">
-    
+
     @include('web.frontend.layouts.head.links')
 
     @include('web.frontend.layouts.head.css')
@@ -19,6 +19,14 @@
     @stack('head-styles')
 
     <link rel="canonical" href="{!! SEOMeta::getCanonical() !!}">
-    
-    @stack('head-scripts')
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap');
+    </style>
+    <style>
+    * {
+        font-family: "Cairo", serif !important;
+    }
+    </style>
+    @st
+    ack('head-scripts')
 </head>
