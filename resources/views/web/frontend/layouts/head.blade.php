c8dba2302a647ef4c980678db70ac6cec26f5ab5
Ali Talaat <alitalaatali30@gmail.com>
2025-01-07 17:50:28 +0200

    update

--- resources/views/web/frontend/layouts/head.blade.php
+++ resources/views/web/frontend/layouts/head.blade.php
@@ -19,14 +19,22 @@
     @stack('head-styles')
 
     <link rel="canonical" href="{!! SEOMeta::getCanonical() !!}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
     <style>
-    @import url('https://fonts.googleapis.com/css2?family=Cairo:wght@200..1000&display=swap');
-    </style>
-    <style>
-    * {
+    body,
+    h1,
+    h2,
+    h3,
+    h4,
+    p,
+    h6,
+    h5 {
         font-family: "Cairo", serif !important;
     }
     </style>
     @st
-    ack('head-scripts')
+    ack
+    ('head-scripts')
 </head>
